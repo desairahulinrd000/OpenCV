f94dd45369a2fcf3b4585a803e43c1ab3c0ccc43
desairahulinrd000 <56038822+desairahulinrd000@users.noreply.github.com>
2020-05-29 19:15:08 +0530

    Add files via upload

---
+++ Negative of an image.py
@@ -0,0 +1,51 @@
+import cv2
+import matplotlib.pyplot as plt
+
+def main():
+    
+    imgpath1="E:\\Sources\\Programming\\Python\\CV2 image processing\\Database image\\misc\\4.2.06.tiff"
+    
+    
+    img = cv2.imread(imgpath1, 1)
+    
+    img1 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    img3 = abs(255-img1)
+    #img3=abs(1-img1)      #for B&W images
+    img4 = abs(255-img2)
+
+    
+    titles = ['Color Image', 'Grayscale', 'Color-Negative', 'Grayscale-Negative']
+    images = [img1, img2, img3, img4]
+    
+    print()
+    
+    plt.subplot(2, 2, 1)
+    plt.imshow(images[0])
+    plt.title(titles[0])
+    plt.xticks([])
+    plt.yticks([])
+    
+    plt.subplot(2, 2, 2)
+    plt.imshow(images[1], cmap='gray')
+    plt.title(titles[1])
+    plt.xticks([])
+    plt.yticks([])
+    
+    plt.subplot(2, 2, 3)
+    plt.imshow(images[2])
+    plt.title(titles[2])
+    plt.xticks([])
+    plt.yticks([])
+    
+    plt.subplot(2, 2, 4)
+    plt.imshow(images[3], cmap='gray')
+    plt.title(titles[3])
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.show()  
+ 
+if __name__ == "__main__":
+    main()
